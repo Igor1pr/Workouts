7336ea37906ca477862a6998a6c2ea157bc13b34
Igor Ramos <62016368+Igor1pr@users.noreply.github.com>
2024-04-01 09:58:55 -0300

    first-commit

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import exercises from './assets/data/exercises.json';
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      {/* Este é um comentário dentro do retorno */}
+      <FlatList
+        data={exercises}
+        renderItem={({ item }) => (
+          <View style={styles.exerciseContainer}>
+            <Text style={styles.exerciseName}>{item.name}</Text>
+            <Text style={styles.exerciseSubtitle}>
+              {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
+            </Text>
+          </View>
+        )}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +27,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gainsboro',
+    padding: 10,
     justifyContent: 'center',
   },
+  exerciseContainer: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    gap: 5,
+  },
+  exerciseName: {
+    fontSize: 20, 
+    fontWeight: '500',
+  },
+  exerciseSubtitle: {
+    color: 'dimgray',
+  }
 });
