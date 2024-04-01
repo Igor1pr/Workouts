2610f447c1129a26fa50db9f22c2f81f93d52caf
Igor Ramos <62016368+Igor1pr@users.noreply.github.com>
2024-04-01 10:45:42 -0300

    Componentização v2
    Agora a componentização feita de forma correta, em um arquivo externo dentro da pasta components

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import exercises from './assets/data/exercises.json';
-
-function ExerciseListitem({ item }) {
-  return (
-    <View style={styles.exerciseContainer}>
-      <Text style={styles.exerciseName}>{item.name}</Text>
-      <Text style={styles.exerciseSubtitle}>
-        {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
-      </Text>
-    </View>
-  )
-}
+import ExerciseListitem from './src/components/ExerciseListItem';
 
 export default function App() {
 
   return (
     <View style={styles.container}>
-      {/* Este é um comentário dentro do retorno */}
       <FlatList
         data={exercises}
+        contentContainerStyle={{ gap: 5 }}
+        // Se a database tivesse uma key ou id para cada item, o KeyExtractor não seria necessário
+        // Caso necessário, é possível passar um index junto com o item, e somar com o item.name para que não corra o risco de haver um nome repetido
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => <ExerciseListitem item={item}/> }
       />
 
@@ -34,18 +27,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'gainsboro',
     padding: 10,
     justifyContent: 'center',
+    paddingTop: 70,
   },
-  exerciseContainer: {
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 10,
-    gap: 5,
-  },
-  exerciseName: {
-    fontSize: 20, 
-    fontWeight: '500',
-  },
-  exerciseSubtitle: {
-    color: 'dimgray',
-  }
 });
